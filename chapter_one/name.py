c2caa976a97e4aaf01c1cec8a5d9e1d8c5e9a162
chenhanghang <1055985520@qq.com>
2026-09-06 15:44:45 +0800

    Changes

---
+++ chapter_one/name.py
@@ -0,0 +1,14 @@
+# name = "ada Lovelace"
+# print(name.title())
+# print(name.upper())
+# print(name.lower())
+# first_name = "ada"
+# last_name = "lovelace"
+# full_name = f"{first_name} {last_name}"
+# print(full_name)
+
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
